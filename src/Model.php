38d911681095875f300a3d684c732d246d74c2de
Georgi Hristov <ghristov@gmx.de>
2020-04-02 14:06:30 +0200

    improve code consistency

--- src/Model.php
+++ src/Model.php
@@ -1257,9 +1257,7 @@ class Model implements \ArrayAccess, \IteratorAggregate
                 }
                 $args[0] = $model->action('count');
 
-                $this->conditions[] = $args;
-
-                return $this;
+                return $this->addCondition(...$args);
             }
 
             $field = $this->getField($field);
