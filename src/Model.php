1c7b1a7ceaa764d7bb3edda5b75e3e1eb83f7aa1
Romans Malinovskis <me@nearly.guru>
2020-04-07 21:27:51 +0000

    Apply fixes from StyleCI

--- src/Model.php
+++ src/Model.php
@@ -14,9 +14,9 @@ use atk4\core\HookTrait;
 use atk4\core\InitializerTrait;
 use atk4\core\NameTrait;
 use atk4\core\ReadableCaptionTrait;
+use atk4\data\Model\Scope\Scope;
 use atk4\data\UserAction\Generic;
 use atk4\dsql\Query;
-use atk4\data\Model\Scope\Scope;
 
 /**
  * Data model class.
@@ -171,7 +171,7 @@ class Model implements \ArrayAccess, \IteratorAggregate
      * @var array
      */
     public $conditions = [];
-    
+
     protected $scope;
 
     /**
@@ -368,7 +368,7 @@ class Model implements \ArrayAccess, \IteratorAggregate
     public function __construct($persistence = null, $defaults = [])
     {
         $this->scope = Model\Scope\Scope::create();
-        
+
         if (is_string($persistence) || is_array($persistence)) {
             $defaults = $persistence;
             $persistence = null;
@@ -1214,7 +1214,7 @@ class Model implements \ArrayAccess, \IteratorAggregate
 
         return $this;
     }
-    
+
     public function scope()
     {
         return $this->scope->setModel($this);
@@ -1724,7 +1724,7 @@ class Model implements \ArrayAccess, \IteratorAggregate
         $default = $field->default;
 
         $scope = clone $this->scope;
-        
+
         // add condition and load record
         $this->addCondition($field_name, $value);
 
@@ -1766,7 +1766,7 @@ class Model implements \ArrayAccess, \IteratorAggregate
         $default = $field_name->default;
 
         $scope = clone $this->scope;
-        
+
         // add condition and try to load record
         $this->addCondition($field_name, $value);
 
